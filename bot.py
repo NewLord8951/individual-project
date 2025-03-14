74568f8343fc9f00c2f1a5224fb28a50c9635a11
Anna <annlovebeauty@gmail.com>
2025-03-14 10:05:44 +0300

    a

--- bot.py
+++ bot.py
@@ -1,34 +1,50 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from loguru import logger
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 
-bot = Bot(TOKEN)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer("Этот quiz по географии пишите всё с маленькой буквы:\
-                         Первый вопрос: Столица Аргентины?")
-
-
-@dp.message()
-async def convers(message: types.Message):
-    if message.text == "буэнос-айрес":
-        await message.answer("Правильно")
-    elif message.text == "Буэнос Айрес":
-        await message.answer("Я же просил")
-    else:
-        await message.answer("Не правильно")
-
 
 async def main():
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days",
+               backtrace=True,
+               diagnose=True)
+
+    bot = Bot(token=TOKEN)
+    logger.info("Бот создан")
+    dp = Dispatcher()
+    logger.info("Диспетчер создан")
+
+    @dp.message(Command("start"))
+    async def send_welcome(message: types.Message):
+        await message.answer("Привет! Получи анекдот по команде: /anekdot")
+        logger.info("Бот запущен")
+
+    @dp.message(Command('anekdot'))
+    async def send_anekdot(message: types.Message):
+        response = requests.get('https://www.anekdot.ru/random/anekdot/')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            jokes = soup.find_all('div', class_='text')
+
+            random_joke = choice(jokes).text.strip()
+            anekdot = random_joke
+        else:
+            anekdot = "Не удалось получить анекдот"
+
+        await message.answer(anekdot)
+
     await dp.start_polling(bot)
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
