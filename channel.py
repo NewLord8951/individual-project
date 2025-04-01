b288a950808684bd87c2ae0533bf3e4f7fcdca7f
Anna <annlovebeauty@gmail.com>
2025-04-01 13:56:31 +0300

    a

--- channel.py
+++ channel.py
@@ -1,23 +1,21 @@
 import asyncio
 import requests
-from aiogram import Dispatcher, F
+from aiogram import Dispatcher, types, F
 from aiogram.filters import Command
 from loguru import logger
 from bs4 import BeautifulSoup
 from random import choice
 
-
 logger.add("file_{time}.log")
 
 
 def channel_joke(dp: Dispatcher):
-    @dp.message(Command('start'), F.chat.type.in_({
-        "channel"}))
-    async def send_random_joke():
+    @dp.message(Command('start'), F.chat.type.in_({"channel"}))
+    async def send_random_joke(message: types.Message):
         while True:
             try:
-                response =\
-                      requests.get("https://www.anekdot.ru/random/anekdot/")
+                response = requests.get(
+                    "https://www.anekdot.ru/random/anekdot/")
                 if response.status_code == 200:
                     soup = BeautifulSoup(response.text, 'html.parser')
                     jokes = soup.find_all('div', class_='text')
@@ -27,7 +25,9 @@ def channel_joke(dp: Dispatcher):
                     anekdot = 'Не удалось получить анекдот'
 
                 logger.info(f"Бот рассказал: {anekdot}")
+
+                await message.answer(anekdot)
             except Exception as e:
-                logger.error(f"Ошибка при отправке сообщения {e}")
+                logger.error(f"Ошибка при отправке сообщения: {e}")
 
             await asyncio.sleep(30)
