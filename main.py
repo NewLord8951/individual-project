625cb745649b1ed8722ce8f9eb70cdfb7f96a44d
New_Lord_8951 <alexandrmaniraki@gmail.com>
2025-04-22 15:32:58 +0300

    Манираки

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # "Qewert1i"
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 
-response = requests.get("https://www.rbc.ru/")
-soup = BeautifulSoup(response.text, "html.parser")
-news = soup.find_all("span", class_="main__feed__title-wrap")
-print([i.text + "\n" for i in news])
+# response = requests.get("https://www.rbc.ru/")
+# soup = BeautifulSoup(response.text, "html.parser")
+# news = soup.find_all("span", class_="main__feed__title-wrap")
+# print([i.text + "\n" for i in news])
